00bd00045c53637b03185002f352f6b8855ee115
youngjinc <cyjin6789@gmail.com>
2023-08-08 22:47:43 +0900

    chore: groovy -> kts로 이전 (#1)

---
+++ app/build.gradle.kts
@@ -0,0 +1,104 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
+val properties = Properties()
+properties.load(project.rootProject.file("local.properties").inputStream())
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("com.google.dagger.hilt.android")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+    alias(libs.plugins.spotless)
+}
+
+android {
+    namespace = "com.hyeeyoung.wishboard"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.hyeeyoung.wishboard"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 30
+        versionName = "1.2.1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("debug") {
+            buildConfigField("String", "BASE_URL", properties.getProperty("DEV_BASE_URL"))
+        }
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            buildConfigField("String", "BASE_URL", properties.getProperty("PROD_BASE_URL"))
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    val composeBom = platform(libs.compose.bom)
+
+    implementation(libs.bundles.androidx)
+    implementation(composeBom)
+    implementation(libs.bundles.compose)
+    implementation(libs.bundles.dagger.hilt)
+    kapt(libs.bundles.compiler)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.bundles.firebase)
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.bundles.network)
+    implementation(libs.bundles.app.update)
+    implementation(libs.junit)
+    implementation(libs.timber)
+
+    coreLibraryDesugaring(libs.desugar)
+    debugImplementation(libs.bundles.debug)
+    testImplementation(libs.bundles.test)
+    androidTestImplementation(composeBom)
+    androidTestImplementation(libs.bundles.android.test)
+}
+
+kapt {
+    correctErrorTypes = true
+}
+
+spotless {
+    kotlin {
+        target("**/*.kt")
+        ktlint("0.48.0").setEditorConfigPath("$rootDir/.editorconfig")
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,5 +12,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
 rootProject.name = "Wishboard"
-include ':app'
+include(":app")
