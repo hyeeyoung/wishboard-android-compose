b5507e29decd7bf66bc6e9f7738c0090e05a93a1
youngjinc <cyjin6789@gmail.com>
2023-08-10 22:54:35 +0900

    chore: 코드 정리 (#2)

--- app/src/main/java/com/hyeeyoung/wishboard/presentation/main/MainActivity.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/presentation/main/MainActivity.kt
@@ -3,24 +3,13 @@ package com.hyeeyoung.wishboard.presentation.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import com.hyeeyoung.wishboard.designsystem.style.WishBoardTheme
-import com.hyeeyoung.wishboard.designsystem.style.WishboardTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WishboardTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = WishBoardTheme.colors.white,
-                ) {
-                    MainScreen()
-                }
-            }
+            // TODO 상태바 컬러 변경, 네비게이션 구현
+            MainScreen()
         }
     }
 }
