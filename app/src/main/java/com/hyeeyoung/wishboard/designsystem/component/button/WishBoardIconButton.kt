0e12f6b226fd7b2529e8630611b4d8ca695bae72
youngjinc <cyjin6789@gmail.com>
2023-08-18 14:38:37 +0900

    fix: 아이콘이 블랙 컬러로 보이는 버그 수정 (#5)

--- app/src/main/java/com/hyeeyoung/wishboard/designsystem/component/button/WishBoardIconButton.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/designsystem/component/button/WishBoardIconButton.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.hyeeyoung.wishboard.util.extension.noRippleClickable
@@ -22,5 +23,6 @@ fun WishBoardIconButton(
             .padding(8.dp),
         painter = painterResource(id = iconRes),
         contentDescription = contentDescription,
+        tint = Color.Unspecified,
     )
 }
