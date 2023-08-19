1868fb986a135961b2fe07fd1202d511db39e293
youngjinc <cyjin6789@gmail.com>
2023-08-19 22:40:03 +0900

    add: 이메일 키보드 옵션 추가 (#5)

--- app/src/main/java/com/hyeeyoung/wishboard/presentation/sign/SignInEmailScreen.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/presentation/sign/SignInEmailScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -12,6 +13,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hyeeyoung.wishboard.R
@@ -46,6 +48,7 @@ fun SignInEmailScreen() {
                     placeholder = stringResource(id = R.string.sign_email_placeholder),
                     errorMsg = stringResource(id = R.string.sign_in_email_error), // TODO 미가입자 에러 메세지 추가
                     onTextChange = {},
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
                 )
 
                 Spacer(modifier = Modifier.weight(1f))
