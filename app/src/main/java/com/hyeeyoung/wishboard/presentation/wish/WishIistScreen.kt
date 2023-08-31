95b5639df6673c510e45bf364e0107b67e0d82c8
youngjinc <cyjin6789@gmail.com>
2023-08-31 19:04:05 +0900

    chore: WishItem 컴포저블 분리 (#13)

--- app/src/main/java/com/hyeeyoung/wishboard/presentation/wish/WishIistScreen.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/presentation/wish/WishIistScreen.kt
@@ -3,41 +3,27 @@ package com.hyeeyoung.wishboard.presentation.wish
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hyeeyoung.wishboard.R
-import com.hyeeyoung.wishboard.designsystem.component.button.CartButton
-import com.hyeeyoung.wishboard.designsystem.component.ColoredImage
 import com.hyeeyoung.wishboard.designsystem.component.button.WishBoardIconButton
 import com.hyeeyoung.wishboard.designsystem.style.WishBoardTheme
 import com.hyeeyoung.wishboard.designsystem.style.WishboardTheme
 import com.hyeeyoung.wishboard.domain.model.WishItem
-import com.hyeeyoung.wishboard.presentation.util.extension.setPriceFormat
+import com.hyeeyoung.wishboard.presentation.wish.component.WishItem
 
 @Composable
 fun WishlistScreen(modifier: Modifier = Modifier) {
@@ -96,7 +82,7 @@ fun WishlistScreen(modifier: Modifier = Modifier) {
                         ),
                     columns = GridCells.Fixed(2),
                 ) {
-                    items(wishList) { wishItem -> WishItemComponent(wishItem = wishItem) }
+                    items(wishList) { wishItem -> WishItem(wishItem = wishItem) }
                 }
             }
         }
@@ -120,55 +106,6 @@ fun WishlistTopBar() { // TODO 공용 TopBar 추출
         Row() {
             WishBoardIconButton(iconRes = R.drawable.ic_cart, onClick = {})
             WishBoardIconButton(iconRes = R.drawable.ic_calendar, onClick = {})
-        }
-    }
-}
-
-@Composable
-fun WishItemComponent(wishItem: WishItem) {
-    Column {
-        var cartState by remember { mutableStateOf(wishItem.isInCart) }
-        // 이미지 및 장바구니 버튼
-        Box {
-            ColoredImage(
-                model = wishItem.imageUrl,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .aspectRatio(1f),
-            )
-
-            Column(
-                modifier = Modifier
-                    .padding(10.dp)
-                    .align(Alignment.BottomEnd),
-            ) {
-                CartButton(
-                    isInCart = cartState,
-                    changeCartState = { isInCart -> cartState = !isInCart },
-                )
-            }
-        }
-
-        // 상품명 및 가격
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 10.dp, end = 10.dp, top = 10.dp, bottom = 20.dp),
-        ) {
-            Text(text = wishItem.name, style = WishBoardTheme.typography.suitD3, color = WishBoardTheme.colors.gray700)
-            Spacer(modifier = Modifier.size(8.dp))
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(
-                    text = wishItem.price.setPriceFormat(),
-                    style = WishBoardTheme.typography.montserratH3,
-                    color = WishBoardTheme.colors.gray700,
-                )
-                Text(
-                    text = stringResource(id = R.string.won),
-                    style = WishBoardTheme.typography.suitD3,
-                    color = WishBoardTheme.colors.gray700,
-                )
-            }
         }
     }
 }
