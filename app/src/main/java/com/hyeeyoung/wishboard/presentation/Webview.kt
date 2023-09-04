2a3b9a832e338d239877a3b92d125b400be4920d
youngjinc <cyjin6789@gmail.com>
2023-09-04 16:19:17 +0900

    design: 웹뷰 상단바 UI 구현 (#26)

--- app/src/main/java/com/hyeeyoung/wishboard/presentation/Webview.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/presentation/Webview.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,7 +14,6 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavHostController
@@ -39,6 +39,12 @@ fun WebViewScreen(
                 title = title ?: "",
                 onClickStartIcon = { navController.popBackStack() },
             ),
+            endComponent = { modifier ->
+                TopBarEndIcon(
+                    modifier = modifier,
+                    onClickRefresh = { webView.value?.reload() },
+                )
+            },
         )
     }) { paddingValues ->
         Column(modifier = Modifier.padding(top = paddingValues.calculateTopPadding())) {
