612eeebed22cbd483841d58843ace0bffc9979b0
youngjinc <cyjin6789@gmail.com>
2023-09-07 18:09:15 +0900

    fix: 일반 등록/수정 뷰에서 하단 영역에 배경 컬러가 하얀색으로 보이지 않는 버그 수정 (#31)

--- app/src/main/java/com/hyeeyoung/wishboard/presentation/upload/WishItemUploadScreen.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/presentation/upload/WishItemUploadScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -90,6 +91,7 @@ fun WishItemUploadScreen(navController: NavHostController, itemDetail: WishItemD
         }) { paddingValues ->
             Column(
                 modifier = Modifier
+                    .fillMaxSize()
                     .background(WishBoardTheme.colors.white)
                     .padding(top = 6.dp + paddingValues.calculateTopPadding(), bottom = 16.dp)
                     .verticalScroll(rememberScrollState()),
