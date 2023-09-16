27b52ff272977cee3242dc5b55f0d8be9a83a2da
youngjinc <cyjin6789@gmail.com>
2023-09-16 16:47:48 +0900

    Merge branch 'dev' into dev-design-withdraw-dialog

--- app/src/main/java/com/hyeeyoung/wishboard/presentation/cart/CartScreen.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/presentation/cart/CartScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -146,6 +147,8 @@ fun CartItem(
                     text = cartItem.name,
                     style = WishBoardTheme.typography.suitD2M,
                     color = WishBoardTheme.colors.gray700,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
                 )
 
                 Surface(modifier = Modifier.padding(top = 6.dp, end = 4.dp)) {
