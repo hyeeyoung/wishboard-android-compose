4bfedaec115afcef1d9670dd9c0919df25405e89
youngjinc <cyjin6789@gmail.com>
2025-04-01 23:32:59 +0900

    add: 커스텀 clickable > enabled  파라미터 추가 (#59)

--- app/src/main/java/com/hyeeyoung/wishboard/presentation/util/extension/ModifierExt.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/presentation/util/extension/ModifierExt.kt
@@ -15,9 +15,10 @@ import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.unit.Dp
 
-inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier = composed {
+inline fun Modifier.noRippleClickable(enabled: Boolean = true, crossinline onClick: () -> Unit): Modifier = composed {
     clickable(
         indication = null,
+        enabled = enabled,
         interactionSource = remember { MutableInteractionSource() },
     ) {
         onClick()
