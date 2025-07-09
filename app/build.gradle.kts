e90bb033437e0166c78d8fd536cd1533e96fc2d8
youngjinc <cyjin6789@gmail.com>
2025-07-09 17:10:01 +0900

    :see_no_evil: 버전코드 41 -> 42 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.hyeeyoung.wishboard"
         minSdk = 24
         targetSdk = 35
-        versionCode = 41
-        versionName = "1.3.0"
+        versionCode = 42
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
