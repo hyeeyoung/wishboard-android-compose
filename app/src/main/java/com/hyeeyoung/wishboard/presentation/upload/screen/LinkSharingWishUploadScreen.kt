fde2e2783ab795d2abc7aee5e85bbb9c957feab3
youngjinc <cyjin6789@gmail.com>
2025-08-30 22:46:22 +0900

    mod: 링크 공유 바텀시트 키보드 위로 올리기

--- app/src/main/java/com/hyeeyoung/wishboard/presentation/upload/screen/LinkSharingWishUploadScreen.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/presentation/upload/screen/LinkSharingWishUploadScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -73,7 +74,7 @@ fun LinkSharingWishUploadScreen(
     onClickSave: () -> Unit,
     onClickClose: () -> Unit = {},
 ) {
-    Box(modifier = Modifier.systemBarsPadding()) {
+    Box(modifier = Modifier.systemBarsPadding().imePadding()) {
         Column(modifier = Modifier.fillMaxSize()) {
             Spacer(
                 modifier = Modifier
