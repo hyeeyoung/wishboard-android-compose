4e85155f3de27be5bc844c045a9369667c9f85bd
youngjinc <cyjin6789@gmail.com>
2025-08-30 21:53:38 +0900

    mod: 마이페이지 하단 영역 배경색이 달라보이는 이슈 수정

--- app/src/main/java/com/hyeeyoung/wishboard/presentation/my/screen/MyScreen.kt
+++ app/src/main/java/com/hyeeyoung/wishboard/presentation/my/screen/MyScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -221,6 +222,7 @@ fun MyScreen(
     ) { paddingValues ->
         LazyColumn(
             modifier = Modifier
+                .fillMaxSize()
                 .background(WishBoardTheme.colors.white)
                 .padding(top = paddingValues.calculateTopPadding()),
             state = lazyListState,
