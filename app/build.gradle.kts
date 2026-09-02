da0131c090d2e47758f15495d751a723577e662f
youngjinc <cyjin6789@gmail.com>
2026-09-02 21:10:44 +0900

    chore: 버전코드 45 -> 46 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.hyeeyoung.wishboard"
         minSdk = 24
         targetSdk = 36
-        versionCode = 45
-        versionName = "3.0.0"
+        versionCode = 46
+        versionName = "3.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
