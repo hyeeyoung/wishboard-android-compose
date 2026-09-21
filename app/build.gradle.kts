8c9eed39cce23fad555083985ba1c2373ffe5320
youngjinc <cyjin6789@gmail.com>
2026-09-21 22:02:11 +0900

    chore: 버전코드 49 -> 50 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.hyeeyoung.wishboard"
         minSdk = 24
         targetSdk = 36
-        versionCode = 49
+        versionCode = 50
         versionName = "3.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
